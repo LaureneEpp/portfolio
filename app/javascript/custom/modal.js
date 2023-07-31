995be60b592223cb49f4237442d7a735eb47e7d1
LaureneEpp <laurene.eppelle@gmail.com>
2023-08-01 00:33:29 +0200

    Add external link to projects + modal if no links

--- app/javascript/custom/modal.js
+++ app/javascript/custom/modal.js
@@ -1,18 +1,35 @@
 // Get all links
-let modal = document.getElementById("my-modal");
-let btn = document.getElementById("open-btn");
-let button = document.getElementById("ok-btn");
+const modal = document.querySelector("#my-modal");
+const project_modals = document.querySelectorAll('.modal-project');
+const contact = document.querySelector("#contact-btn");
+const ok_btns = document.querySelectorAll(".ok-btn");
+const ok_btn = document.querySelector("#ok-btn");
+const project_btns = document.querySelectorAll(".project-btn")
 
 // Display modal centered vertically and horizontally when click on contact
-btn.addEventListener('click', () => {
+contact.addEventListener('click', () => {
     modal.style.display = "flex";
     modal.classList.add('justify-center', 'items-center')
+    console.log('Test for button-modal')
 })
 
+// Display modal centered vertically and horizontally when click on project button
+project_btns.forEach(project => project.addEventListener('click', (e) => {
+    const project_modal = document.querySelector('.modal-project');
+    const button = document.querySelector(".ok-btn");
+    project_modal.style.display = "flex";
+    project_modal.classList.add('justify-center', 'items-center')
+    console.log('Test for button-modal')
+} ))
+
 // Modal hidden when click on ok and anywhere on the page
-button.addEventListener('click', () => {
+ok_btn.addEventListener('click', () => {
     modal.style.display = "none";
 })
+ok_btns.forEach(ok_btn => ok_btn.addEventListener('click', (e) => {
+    const project_modal = document.querySelector('.modal-project');
+    project_modal.style.display = "none";
+} ))
 
 window.addEventListener('click', (event) => {
     if (event.target == modal) {
@@ -21,3 +38,4 @@ window.addEventListener('click', (event) => {
 })
  
 console.log('Test for modal')
+
