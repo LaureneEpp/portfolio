56da9229656493a26a11a2b09c479ee32210873a
Laurène Eppellé <Laurene@MacBook-Pro-de-Eppelle.local>
2024-03-30 18:52:07 +0100

    install rails as backend

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::PagesController < ApplicationController
+    def home
+        render json: @page
+    end
+    
+    def cv
+        render json: @page
+    end
+  
+    def projects
+        render json: @page
+    end
+  
+    def skills
+        render json: @page
+    end
+end
