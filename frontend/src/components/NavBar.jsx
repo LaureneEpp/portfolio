801a62d1d246c57daa0d8d6d5c230350f3ff616f
Laurène Eppellé <Laurene@MacBook-Pro-de-Eppelle.local>
2024-04-03 12:35:51 +0200

    setup bootstrap

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <nav>
+      <button type="button" className="btn btn-primary">
+        Primary
+      </button>
     </nav>
   );
 }
